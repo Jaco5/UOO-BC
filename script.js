a23a9ca8c941e49cc97d90416aa8ef3bce53a11a
Jake <jacobartz1987@gmail.com>
2019-03-02 22:30:40 -0700

    basic setup

---
+++ script.js
@@ -0,0 +1,11 @@
+var dmgBonus = 0;
+var skills = {
+    anatomy: .25,
+    blacksmithing: .15, 
+    camping: .25,
+    forensicEval: .25,
+    mining: .25,
+    tactics: .5,
+    tracking: .25,
+};
+
